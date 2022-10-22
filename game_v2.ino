ec775596ca24c5acc8e8c85c5b396d1978082351
soph2122 <113772195+soph2122@users.noreply.github.com>
2022-10-22 12:16:20 +0200

    Update game_v2.ino

--- game_v2.ino
+++ game_v2.ino
@@ -1,285 +1,259 @@
 //Missing LCD Screen, Highscore Function /Able to save highscore.
 
-//============================================variables==========================================
-
-int gameModeChoice = 1; 
+int gameModeChoice = 1;
 
 bool compareSentence = true;
 
-int LEDRed = 8;
-int LEDBlue = 11;
-int LEDGreen = 10;
-int LEDYellow = 9;
+int LEDRed = 0;
+int LEDBlue = 15;
+int LEDGreen = 16;
+int LEDYellow = 2;
 int buttonYellow = 3;
-int buttonGreen = 4;
-int buttonRed = 2;
-int buttonBlue = 1;
+int buttonGreen = 12;
+int buttonRed = A0;
+int buttonBlue = 14;
+//int buttonReset = D0;
 
-int buzzerPin = 6; 
+int buzzerPin = 13;
 
-int j=0;
+int j = 0;
 
-const long lightUpTime = 1000;
+const long lightUpTime = 500;
 const long inputTime = 5000;
 
 int skill;
 int count = 0;
-int currentSentence[32]= {0};
-bool newWord;
+int currentSentence[32] = {0};
+int ind = 0;
+
 bool chosen = false;
-bool skillChosen = false;
-
-
-//=============================================functions==========================================
-
-int gameMode() {
-  Serial.println("Welcome to Simons Game. Please chose a game mode number.");
-  Serial.println("    1. Single Player");
-  Serial.println("    2. Multi Player - adding colors alternately");
-  Serial.println("    1. Multi Player - responsible for a color each");
-
-  /*while (Serial.available() == 0) {
-  }*/
-
-  while(chosen == false){
-    if(buttonRed == HIGH){
-      int gameModeChoice = 1;
-      chosen = true;
-    }
-    else if(buttonYellow == HIGH){
-      int gameModeChoice = 2;
-      chosen = true;
-    }
-    else if(buttonGreen == HIGH){
-      int gameModeChoice = 3;
-      chosen = true;
-    }
-  }
-
-  //int gameModeChoice = Serial.parseInt();
-
-  return gameModeChoice;  
-}
+bool skillChosen;
+bool resett = false;
 
 int skillLevel() {
-  Serial.println("You have chosen gamemode 1. Please choose skill level from 1-4");
+  delay(300);
+  Serial.println("Please choose skill level from 1-4");
   Serial.println("    1. Repeat a sequence of 8 colours");
   Serial.println("    2. Repeat a sequence of 14 colours");
   Serial.println("    3. Repeat a sequence of 20 colours");
   Serial.println("    4. Repeat a sequence of 31 colours");
+  skillChosen = false;
 
-  while(skillChosen == false){
-    if(buttonRed == HIGH){
-      int skill = 8;
+  while (skillChosen == false) {
+    delay(10);
+    //Serial.println(analogRead(buttonRed));
+
+    if (analogRead(buttonRed) == 1024) {
+      skill = 8;
       skillChosen = true;
     }
-    else if(buttonYellow == HIGH){
-      int skill = 16;
+    else if (digitalRead(buttonYellow) == LOW) {
+      skill = 16;
       skillChosen = true;
     }
-    else if(buttonGreen == HIGH){
-      int skill = 24;
+    else if (digitalRead(buttonGreen) == LOW) {
+      skill = 24;
       skillChosen = true;
     }
-    else if(buttonBlue == HIGH){
-      int skill = 32;
+    else if (digitalRead(buttonBlue) == LOW) {
+      skill = 32;
       skillChosen = true;
     }
+  }
+  for (int i = 0; i < skill; i++) {
+      currentSentence[i]=0;
+      //Serial.println(currentSentence[i]);
   }
   return skill;
 }
 
 int * SentenceAdd(int *  current_sentence) {
-  int ran_number = rand() % 4 +1;
-  current_sentence[count] = ran_number;
-  count +=1;
+  int randNumber = random(1, 5);
+  current_sentence[count] = randNumber;
+  count += 1;
   return current_sentence;
 }
 
-bool compare_sentence(int * sentence, int input, int index){
-  if (sentence[index] == input) {
+void lightLED(int LED, int previousMillis, int freq) {
+  //Serial.println("light");
+  digitalWrite(LED, HIGH);
+  tone(buzzerPin, freq);
+  unsigned long currentMillis = millis();
+  while (currentMillis - previousMillis <= lightUpTime) {
+    currentMillis = millis();
+  }
+  digitalWrite(LED, LOW);
+  noTone(buzzerPin);
+  delay(500);
+  //newWord=true;
+}
+
+boolean simonSpeaks(int * sentence, int length) {
+
+  for (int pos = 0; pos < length; pos++) {
+    int element = sentence[pos];
+    unsigned long previousMillis = millis();
+
+    if (element == 0) {
+      return element;
+    }
+
+    else if (element == 1) {
+      lightLED(LEDRed, previousMillis, 4000);
+    }
+
+    else if (element == 2) {
+      lightLED(LEDYellow, previousMillis, 2000);
+    }
+
+    else if (element == 3) {
+      lightLED(LEDBlue, previousMillis, 1000);
+    }
+
+    else if (element == 4) {
+      lightLED(LEDGreen, previousMillis, 3000);
+    }
+  }
+}
+
+int userSentenceInput() {
+  long previousInputMillis = millis();
+  long inputMillis = millis();
+
+  while (inputMillis - previousInputMillis <= inputTime) {
+    inputMillis = millis();
+    delay(200);
+    if (digitalRead(buttonYellow) == LOW) {
+      while (digitalRead(buttonYellow) == LOW) {
+        digitalWrite(LEDYellow, HIGH);
+        tone(buzzerPin, 2000);
+      }
+      delay(200);
+      digitalWrite(LEDYellow, LOW);
+      noTone(buzzerPin);
+      return 2;
+    }
+    else if (digitalRead(buttonGreen) == LOW) {
+      while (digitalRead(buttonGreen) == LOW) {
+        digitalWrite(LEDGreen, HIGH);
+        tone(buzzerPin, 3000);
+      }
+      delay(200);
+      digitalWrite(LEDGreen, LOW);
+      noTone(buzzerPin);
+      return 4;
+    }
+    else if (analogRead(buttonRed) == 1024) {
+      while (analogRead(buttonRed) == 1024) {
+        digitalWrite(LEDRed, HIGH);
+        tone(buzzerPin, 4000);
+      }
+      digitalWrite(LEDRed, LOW);
+      noTone(buzzerPin);
+      delay(200);
+      return 1;
+    }
+    else if (digitalRead(buttonBlue) == LOW) {
+      while (digitalRead(buttonBlue) == LOW) {
+        digitalWrite(LEDBlue, HIGH);
+        tone(buzzerPin, 1000);
+      }
+      digitalWrite(LEDBlue, LOW);
+      noTone(buzzerPin);
+      delay(200);
+      return 3;
+    }
+  }
+  delay(200);
+  //Serial.print("5");
+  return 5;
+}
+
+bool compare_sentence(int * sentence, int input, int ind) {
+  if (sentence[ind] == input) {
     return true;
   }
-  else{
+  else {
     return false;
   }
 }
 
-//user input function
-int userSentenceInput(){
-  long previousInputMillis = millis();
-  long inputMillis = millis();
 
-  while (inputMillis - previousInputMillis >= inputTime){
-    inputMillis = millis();
-    if (digitalRead(buttonYellow) == HIGH){
-      while(digitalRead(buttonYellow) == HIGH){
-        digitalWrite(LEDYellow,HIGH);
-        tone(buzzerPin,1000);
-      }
-      digitalWrite(LEDYellow,LOW);
-      noTone(buzzerPin);
-      return 1;
-    }
-    else if (digitalRead(buttonGreen) == HIGH){
-      while(digitalRead(buttonGreen) == HIGH){
-        digitalWrite(LEDGreen,HIGH);
-        tone(buzzerPin,2000);
-      }
-      digitalWrite(LEDGreen,LOW);
-      noTone(buzzerPin);
-      return 2;
-    }
-    else if (digitalRead(buttonRed) == HIGH){
-      while(digitalRead(buttonRed) == HIGH){
-        digitalWrite(LEDRed,HIGH);
-        tone(buzzerPin,3000);
-      }
-      digitalWrite(LEDRed,LOW);
-      noTone(buzzerPin);
-      return 3;
-    }
-    else if (digitalRead(buttonBlue) == HIGH){
-      while(digitalRead(buttonBlue) == HIGH){
-        digitalWrite(LEDBlue,HIGH);
-        tone(buzzerPin,4000);
-      }
-      digitalWrite(LEDBlue,LOW);
-      noTone(buzzerPin);
-      return 4;
-    }
-  }
-  return 5; 
-}
-
-void lightLED(int LED,int previousMillis, int freq){
-  digitalWrite(HIGH,LED);
-  tone(buzzerPin,freq);
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= lightUpTime){
-    digitalWrite(LOW,LED);
-    noTone(buzzerPin);
-    newWord = true;   
-    }
-}
-
-boolean simonSpeaks(int * sentence, int length){
-  for (int pos = 0; pos < length; pos++){
-    int element = sentence[pos];
-    boolean newWord = false;
-    switch(element){
-      unsigned long previousMillis = millis();
-      case 0: {
-        return false;
-        }
-        
-      case 1: {
-        while(newWord = false){
-          lightLED(LEDBlue,previousMillis,1000);
-          }
-        }
-        break;
-        
-      case 2: {
-        while(newWord = false){
-          lightLED(LEDGreen,previousMillis,2000);
-        }}
-        break;
-        
-      case 3: {
-        while(newWord = false){
-          lightLED(LEDYellow,previousMillis,3000);
-          }
-        }
-        break;
-      case 4: {
-        while(newWord = false){
-          lightLED(LEDRed,previousMillis,4000);
-          }
-        }
-        break;
-    }
-  }
-}
-
-
-//=============================run the game with all functions=========================================
 boolean game() {
-  
-  switch (gameMode()) {
-    case 1: {
-      //Could insert a while loop of which is entered when the case is first fulfill
-      int sentenceLength = skillLevel();
-      int * currentSentence = SentenceAdd(currentSentence);
+  int sentenceLength = skillLevel();
+  ind = 0;
+  Serial.print(sentenceLength);
+  Serial.print(ind);
 
-      simonSpeaks(currentSentence,sentenceLength);
+  while (ind < sentenceLength) {
+    Serial.print(ind);
+    j = 0;
+    SentenceAdd(currentSentence);
+    delay(50);
+    for (int i = 0; i < sentenceLength; i++) {
+      //Serial.println(currentSentence[i]);
+    }
 
-      while (currentSentence[j]!=0)
-      {
-        int input = userSentenceInput();
-        if(compare_sentence(currentSentence,input,j)==true){
-          j++;
-        }
-        else{
-          return false;
-        }
 
+    simonSpeaks(currentSentence, sentenceLength);
+
+    while (currentSentence[j] != 0) {
+      int input = userSentenceInput();
+      delay(200);
+      if (compare_sentence(currentSentence, input, j) == true) {
+        //Serial.print("correct");
+        j++;
       }
-
-      if (j >= sentenceLength)
-      {
-        return true;
-      }
-
-    } 
-      break;
-
-    case 2: {
-      Serial.println("You have chosen gamemode 2.");
-      //run function of gamemode 2
-      if (compareSentence == true){
-        return true;
-      }
-
       else {
+        //Serial.println("you lose");
         return false;
       }
     }
-    break;
 
-    case 3: {
-      Serial.println("You have chosen gamemode 3.");
-      //run function of gamemode 2'
-      if (compareSentence == true){
-        return true;
-      }
+    delay(300);
+    ind++;
+  }
 
-      else {
-        return false;
-      }
-    }
-    break;
-
-    default: {
-        Serial.println("Please choose a valid option. Valid options are 1, 2 or 3.");
-      }
+  if (j >= sentenceLength) {
+    //Serial.println("congrats");
+    return true;
   }
 }
 
+void res() {
+  while (resett != true) {
+    delay(50);
+    if (digitalRead(buttonRed) == 1024) {
+      resett = true;
+      j = 0;
+      skill = 0;
+      count = 0;
+      currentSentence[32] = {0};
+      chosen = false;
+      skillChosen = false;
+      ind = 0;
+    }
+  }
+  delay(50);
+  resett = false;
+}
 
 void setup() {
-
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinMode(LEDRed, OUTPUT);
   pinMode(LEDBlue, OUTPUT);
   pinMode(LEDGreen, OUTPUT);
   pinMode(LEDYellow, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 
-  pinMode(buttonRed, INPUT_PULLUP);
+  pinMode(buttonRed, INPUT);
   pinMode(buttonBlue, INPUT_PULLUP);
   pinMode(buttonGreen, INPUT_PULLUP);
   pinMode(buttonYellow, INPUT_PULLUP);
+  //pinMode(buttonReset, INPUT_PULLUP);
+
+  randomSeed(analogRead(0));
 
 }
 
@@ -287,13 +261,30 @@ void loop() {
 
   bool ending = game();
 
-  if (ending == true){
-    //Player won
-  }
-  else if(ending == false){
-    //Player Lost
-  }
+  if (ending == true) {
+    //Serial.print("YOU WON");
+    j = 0;
+    skill;
+    count = 0;
+    currentSentence[32] = {0};
+    ind = 0;
 
-  // put your main code here, to run repeatedly:
+    chosen = false;
+    skillChosen = false;
+  }
+  else if (ending == false) {
+    //Serial.print("YOU LOST");
+    j = 0;
+    skill;
+    count = 0;
+    currentSentence[32] = {0};
+    ind = 0;
+
+    chosen = false;
+    skillChosen = false;
+  }
+  //game();
+  delay(500);
+
 
 }
